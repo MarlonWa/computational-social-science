7b63c6cadb42df501085486072871fcaa7eeba15
marlon <marlon.wachsmuth@tum.de>
2025-11-28 14:46:20 +0100

    in progress 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ResponsiveAppBar from './Header.jsx'
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxWidth="100%">
+        <ResponsiveAppBar/>
+      </Container>
+
+      <Container>
+          <TextField
+            id="outlined-multiline-static"
+            label="placeholder text"
+            fullWidth
+            multiline
+            minRows={6}
+            defaultValue="Default Value"
+            slotProps={{
+              textarea: {
+                style: {
+                  maxHeight: "40h%",
+                  overflowY: "auto",
+                },
+              },
+            }}
+            />
+      </Container>
+<Container></Container>
     </>
   )
 }
